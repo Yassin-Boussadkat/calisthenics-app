1fbdbb3c94ae1ff377bfd4d4c5ca41d98e8ada97
yassi <valid@com>
2026-02-28 12:58:09 +0100

    Adds ExerciseService

--- src/main/java/com/bous/calisthenics_app/service/ExerciseService.java
+++ src/main/java/com/bous/calisthenics_app/service/ExerciseService.java
@@ -1,6 +1,48 @@
 package com.bous.calisthenics_app.service;
 
+import com.bous.calisthenics_app.entity.DifficultyLevel;
+import com.bous.calisthenics_app.entity.Exercise;
+import com.bous.calisthenics_app.entity.ExerciseType;
+import com.bous.calisthenics_app.entity.MuscleGroup;
+import com.bous.calisthenics_app.repository.ExerciseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ExerciseService {
 
+    private final ExerciseRepository exerciseRepository;
 
+    public ExerciseService(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public List<Exercise> findAll(){
+        return exerciseRepository.findAll();
+    }
+
+    public Exercise save(Exercise exercise){
+        return exerciseRepository.save(exercise);
+    }
+
+    public void deleteById(Long id){
+        exerciseRepository.deleteById(id);
+    }
+
+    public List<Exercise> findByDifficultyLevel(DifficultyLevel difficultyLevel) {
+        return exerciseRepository.findByDifficultyLevel(difficultyLevel);
+    }
+
+    public List<Exercise> findByName(String name){
+        return exerciseRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public List<Exercise> findByMuscleGroupsContaining(MuscleGroup muscleGroup){
+        return exerciseRepository.findByMuscleGroupsContaining(muscleGroup);
+    }
+
+    public List<Exercise> findByExerciseType(ExerciseType exerciseType){
+        return exerciseRepository.findByExerciseType(exerciseType);
+    }
 }
