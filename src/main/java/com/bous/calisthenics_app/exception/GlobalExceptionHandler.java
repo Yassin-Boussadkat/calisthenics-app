91f9df913734d216f936df592babb3491d26b61a
Yassin <yassinboussadkat@hotmail.com>
2026-07-21 19:43:19 +0200

    load JWT secret from env var and return 401 on bad login credentials

--- src/main/java/com/bous/calisthenics_app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bous/calisthenics_app/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.bous.calisthenics_app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,16 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                "Ongeldige email of wachtwoord."
+        );
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex) {
